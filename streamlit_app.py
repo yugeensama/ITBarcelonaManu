31d9b6487f8e3157a66b4ab54af8f0238be2a5bc
yugeensama <124042488+yugeensama@users.noreply.github.com>
2024-08-20 10:33:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,5 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+>>> a = st.sidebar.radio('Choose:',[1,2])
 st.button('Hit me')
