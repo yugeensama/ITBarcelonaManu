e31386327442e79433d2aa3e9ce4785f0f5f5063
yugeensama <124042488+yugeensama@users.noreply.github.com>
2024-08-20 10:34:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,6 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+>>> if st.sidebar.selectbox('I:',['f']) == 'f': b()
 >>> a = st.sidebar.radio('Choose:',[1,2])
 st.button('Hit me')
